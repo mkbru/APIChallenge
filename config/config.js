c8c634576fd1585b217ebfe772ffd818e32862c8
mikebrusilov <mikebrusilov@gmail.com>
2018-10-11 11:49:51 -0400

    readme updated

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    dbhost: 'sql9.freemysqlhosting.net',
-    dbuser: 'sql9256245',
-    dbdatabase: 'sql9256245',
-    dbpassword: 'nIVhMgZBEV',
+    dbhost: '35.232.227.254',
+    dbuser: 'mkbru',
+    dbdatabase: 'testdb',
+    dbpassword: 'ping1234',
 
     testingdbhost: 'sql9.freemysqlhosting.net',
     testingdbuser: 'sql9256985',
@@ -10,4 +10,11 @@ module.exports = {
     testingdbpassword: 'KRCS5zPGZ7',
     secret: 'qQWrg*Ws+/MS2#>!QQf89r!(8g~ePA',
     PORT: 8080
-}
+};
+
+
+//
+// dbhost: '35.232.227.254',
+//     dbuser: 'mkbru',
+//     dbdatabase: 'testdb',
+//     dbpassword: 'ping1234',
